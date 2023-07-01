a1c04d6845591bbaa4c61d5ef1e3d5ecb9aaa29a
FoxyRaspberry <hogwartsgsrh97@gmail.comm>
2023-07-01 18:06:56 +0300

    init: add API key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '3f526e12e7ae41e897f823bd1c2581de',
         });
     }
 }
